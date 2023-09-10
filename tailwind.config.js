f50432bdf01ac836e2f226ba590514fc78139436
Moonbrite <josemiguelvalecosta@gmail.com>
2023-09-10 20:13:35 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "gris-de-ouf": "#f6f6f6",
+      },
+      minWidth: {
+        '1/2': '45%',
+      },
+    },
+  },
+  plugins: [],
+}
+
